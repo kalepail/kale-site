7d1f5be51c328f25f5f69944e86247eb496e9a2b
Tyler van der Hoeven <hi@tyvdh.com>
2024-12-17 14:58:14 -0500

    small tweaks to leaderboard

--- src/components/Leaderboard.svelte
+++ src/components/Leaderboard.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import { truncate } from "../utils/base";
+    import { contractId } from "../store/contractId";
 
     let leaderboard: {
         balance_holder: string;
@@ -22,19 +23,29 @@
 </script>
 
 <h1 class="text-xl font-bold mb-2">
-    Leaderboard
+    Leaderboard <span class="text-sm">({leaderboard.length})</span>
 </h1>
 
 <ul>
     {#each leaderboard as { balance_holder, balance_amount }, i}
         <li class="font-mono">
-            {i + 1}.
+            {#if i < 3}
+                {i === 0 ? '🥇' : ''}
+                {i === 1 ? '🥈' : ''}
+                {i === 2 ? '🥉' : ''}
+            {:else}
+                {i + 1}.
+            {/if}
             <a
                 class="underline"
                 href={`https://stellar.expert/explorer/public/${balance_holder[0] === 'G' ? 'account' : 'contract'}/${balance_holder}`}
                 target="_blank">{truncate(balance_holder, 8)}</a
             >
             : {balance_amount}
+            
+            {#if balance_holder === $contractId}
+                {balance_holder === $contractId ? '🫵' : ''}
+            {/if}
         </li>
     {/each}
 </ul>
